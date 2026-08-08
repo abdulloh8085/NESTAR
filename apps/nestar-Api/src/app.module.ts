7e812b64d20a49beda9006ffdbf6e465e8ce91c3
Abdulloh <abdulloh.im4@gmail.com>
2026-08-08 18:55:03 +0900

    Feat: build project Standards

--- apps/nestar-Api/src/app.module.ts
+++ apps/nestar-Api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {} from "@nestjs/config";
 import { ConfigModule } from '@nestjs/config';
 import {GraphQLModule} from "@nestjs/graphql";
 import {ApolloDriver} from "@nestjs/apollo" 
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
@@ -13,7 +14,9 @@ import { AppResolver } from './app.resolver';
     playground: true, 
     uploads: false, 
     autoSchemaFile: true,
-  })],
+  }), 
+  ComponentsModule, DatabaseModule,
+],
   controllers: [AppController],
   providers: [AppService, AppResolver],
 })
---
+++ apps/nestar-Api/src/libs/enums/common.enum.ts
@@ -0,0 +1,19 @@
+export enum Message {
+   SOMETHING_WENT_WRONG = 'Something went wrong!',
+   NO_DATA_FOUND = 'No data found!', 
+   CREATE_FAILED = 'Create Failed',
+   UPDATE_FAILED = 'Update failed',
+   REMOVE_FAILED = 'Remove failded',
+   UPLOAD_FAILED = 'Upload Failed',
+   BAD_REQUEST = 'Bad Request',
+
+   NO_MEMBER_NICK = 'OUCH ERR!',
+   BLOCKED_USER = 'OUCH ERR!',
+   WRONG_PASSWORD = 'OUCH ERR!',
+   NOT_AUTHENTICATED = 'OUCH ERR!',
+   TOKEN_NOT_EXISTS = 'OUCH ERR!',
+   ONLY_SPECIFIC_ROLES_ALLOWED = 'OUCH ERR!',
+   NOT_ALLOWED_REQUEST = 'OUCH ERR!',
+   PROVEDE_ALLOWED_FORMAT = 'OUCH ERR!',
+   SELF_SUBSCRIPTION_DENIED = 'OUCH ERR!',
+}
