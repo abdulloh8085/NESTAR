7719db17f0be037616116f8fb7259bf9fe90411b
Abdulloh <abdulloh.im4@gmail.com>
2026-08-18 18:38:43 +0900

    Feat: develop global error, GraphQL error handling system

--- apps/nestar-Api/src/app.module.ts
+++ apps/nestar-Api/src/app.module.ts
@@ -2,22 +2,42 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import {GraphQLModule} from "@nestjs/graphql";
-import {ApolloDriver} from "@nestjs/apollo" 
 import { AppResolver } from './app.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo'
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { T } from './libs/types/common';
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
-    driver: ApolloDriver,
-    playground: true, 
-    uploads: false, 
-    autoSchemaFile: true,
-  }), 
-  ComponentsModule, DatabaseModule,
-],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playgroung: true,
+      uploads: false,
+      autoSchemaFile: true,
+      formatError: (error: T) => {
+        const graphQLFormattedError = {
+          code: error?.extensions.code,
+          message:
+            error?.extensions?.exception?.response?.message ||
+            error?.extensions?.response?.message ||
+            error?.message,
+          extensions: {
+            code: error?.extensions.code,
+          },
+        };
+
+        console.log("GRAPHQL GLOBAL ERR:", graphQLFormattedError);
+        return graphQLFormattedError;
+      },
+    }),
+    ComponentsModule,
+    DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
 })
-export class AppModule {}
+export class AppModule { }
