caf7e86d3ca410c89cd2d8ffae5680a50b4f99b6
Abdulloh <abdulloh.im4@gmail.com>
2026-08-29 19:10:43 +0900

    feat: develop getProperties business logic

--- apps/nestar-Api/src/libs/config.ts
+++ apps/nestar-Api/src/libs/config.ts
@@ -1,16 +1,37 @@
 import { ObjectId } from "bson";
-
-
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberVievs', 'memberRank']
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberVievs']
-// IMAGE CONFIGURATION (config.js)
-import { v4 as uuidv4 } from 'uuid';
+
+export const availableOptions = ['propertyBarter', 'propertyRent'];
+
+export const availablePropertySorts = [
+    'createdAt',
+    'updatedAt',
+    'propertyLikes',
+    'propertyViews',
+    'propertyRank',
+    'propertyPrice',
+];
+
+/** IMAGE CONFIGURATION **/
 import * as path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
     const ext = path.parse(filename).ext;
     return uuidv4() + ext;
 };
+
 export const shapeIntoMongoObjectId = (target: any) => {
-    return typeof target === "string" ? new ObjectId(target) : target}
+    return typeof target === "string" ? new ObjectId(target) : target;
+};
+
+export const lookupMember = {
+    $lookup: {
+        from: 'members',
+        localField: 'memberId',
+        foreignField: '_id',
+        as: 'memberData',
+    },
+};
--- apps/nestar-Api/src/libs/dto/property/property.input.ts
+++ apps/nestar-Api/src/libs/dto/property/property.input.ts
@@ -1,71 +1,167 @@
-import { Field, InputType, Int } from "@nestjs/graphql";
-import { IsIn, IsInt, IsNotEmpty, IsOptional, Length, Min } from "class-validator";
-import { PropertyLocation, PropertyType } from "../../enums/property.enum";
-import type { ObjectId } from "mongoose";
-
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsIn, IsInt, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
+import { PropertyLocation, PropertyType } from '../../enums/property.enum';
+import { ObjectId } from 'mongoose';
+import { availableOptions, availablePropertySorts } from '../../config';
+import { Direction } from '../../enums/common.enum';
 
 @InputType()
 export class PropertyInput {
-
     @IsNotEmpty()
     @Field(() => PropertyType)
-    propertyType!: PropertyType
+    propertyType: PropertyType;
 
     @IsNotEmpty()
     @Field(() => PropertyLocation)
-    propertyLocation!: PropertyLocation
+    propertyLocation: PropertyLocation;
 
     @IsNotEmpty()
     @Length(3, 100)
     @Field(() => String)
-    propertyAddress!: PropertyType
+    propertyAddress: string;
 
     @IsNotEmpty()
     @Length(3, 100)
     @Field(() => String)
-    propertyTitle!: PropertyType
+    propertyTitle: string;
 
     @IsNotEmpty()
     @Field(() => Number)
-    propertyPrice!: number
+    propertyPrice: number;
 
     @IsNotEmpty()
     @Field(() => Number)
-    propertySquare!: number
+    propertySquare: number;
 
     @IsNotEmpty()
     @IsInt()
     @Min(1)
     @Field(() => Int)
-    propertyBeds!: number
+    propertyBeds: number;
 
     @IsNotEmpty()
     @IsInt()
     @Min(1)
     @Field(() => Int)
-    propertyRooms!: number
+    propertyRooms: number;
 
     @IsNotEmpty()
     @Field(() => [String])
-    propertyImages!: string[]
+    propertyImages: string[];
 
     @IsOptional()
     @Length(5, 500)
     @Field(() => String, { nullable: true })
-    propertyDesc?: string
+    propertyDesc?: string;
 
     @IsOptional()
     @Field(() => Boolean, { nullable: true })
-    propertyBarter?: boolean
+    propertyBarter?: boolean;
 
     @IsOptional()
     @Field(() => Boolean, { nullable: true })
-    propertyRent?: boolean
+    propertyRent?: boolean;
 
-
-    memberId?: ObjectId
+    memberId?: ObjectId;
 
     @IsOptional()
     @Field(() => Date, { nullable: true })
-    constructedAt?: Date
+    constructedAt?: Date;
+}
+
+@InputType()
+export class PricesRange {
+    @Field(() => Int)
+    start: number;
+
+    @Field(() => Int)
+    end: number;
+}
+
+@InputType()
+export class SquaresRange {
+    @Field(() => Int)
+    start: number;
+
+    @Field(() => Int)
+    end: number;
+}
+
+@InputType()
+export class PeriodsRange {
+    @Field(() => Date)
+    start: Date;
+
+    @Field(() => Date)
+    end: Date;
+}
+
+@InputType()
+export class PISearch {
+    @IsOptional()
+    @Field(() => String, { nullable: true })
+    memberId?: ObjectId;
+
+    @IsOptional()
+    @Field(() => PropertyLocation, { nullable: true })
+    locationList?: PropertyLocation;
+
+    @IsOptional()
+    @Field(() => [PropertyType], { nullable: true })
+    typeList?: PropertyType[];
+
+    @IsOptional()
+    @Field(() => [Int], { nullable: true })
+    roomsList?: Number[];
+
+    @IsOptional()
+    @Field(() => [Int], { nullable: true })
+    bedsList?: Number[];
+
+    @IsOptional()
+    @IsIn(availableOptions, { each: true })
+    @Field(() => [String], { nullable: true })
+    options?: string[];
+
+    @IsOptional()
+    @Field(() => PricesRange, { nullable: true })
+    pricesRange?: PricesRange;
+
+    @IsOptional()
+    @Field(() => PeriodsRange, { nullable: true })
+    periodsRange?: PeriodsRange;
+
+    @IsOptional()
+    @Field(() => SquaresRange, { nullable: true })
+    squaresRange?: SquaresRange;
+
+    @IsOptional()
+    @Field(() => String, { nullable: true })
+    text?: string;
+}
+
+@InputType()
+export class PropertiesInquiry {
+    @IsNotEmpty()
+    @Min(1)
+    @Field(() => Int)
+    page: number;
+
+    @IsNotEmpty()
+    @Min(1)
+    @Field(() => Int)
+    limit: number;
+
+    @IsOptional()
+    @IsIn(availablePropertySorts)
+    @Field(() => String, { nullable: true })
+    sort?: string;
+
+    @IsOptional()
+    @Field(() => Direction, { nullable: true })
+    direction?: Direction;
+
+    @IsNotEmpty()
+    @Field(() => PISearch)
+    search: PISearch;
 }
