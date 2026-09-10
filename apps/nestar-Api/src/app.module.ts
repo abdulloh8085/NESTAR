a04f01225fbafd47002fd16e8a02266c84983669
Abdulloh <abdulloh.im4@gmail.com>
2026-09-10 18:42:02 +0900

    Feat: WebSocket creating Process

--- apps/nestar-Api/src/app.module.ts
+++ apps/nestar-Api/src/app.module.ts
@@ -8,6 +8,7 @@ import { ApolloDriver } from '@nestjs/apollo'
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { T } from './libs/types/common';
+import { SocketModule } from './socket/socket.module';
 
 
 @Module({
@@ -16,7 +17,7 @@ import { T } from './libs/types/common';
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       playgroung: true,
-      uploads: true,
+      uploads: false,
       autoSchemaFile: true,
       formatError: (error: T) => {
         const graphQLFormattedError = {
@@ -35,7 +36,8 @@ import { T } from './libs/types/common';
       },
     }),
     ComponentsModule,
-    DatabaseModule
+    DatabaseModule,
+    SocketModule
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
